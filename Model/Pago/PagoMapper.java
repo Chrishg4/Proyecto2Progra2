21b40ffda8e57b8f890d9e4318e28547c98b57d1
Keizel <pamelamarchena76@gmail.com>
2024-11-28 18:18:47 -0600

    Se arregla la clase PagoMapper

--- Model/Pago/PagoMapper.java
+++ Model/Pago/PagoMapper.java
@@ -4,7 +4,14 @@
  */
 package Model.Pago;
 
+import Database.Database;
+import Model.Customer.CustomerDAO;
+import Model.Customer.CustomerMapper;
 import Model.Mapper.Mapper;
+import Model.Pago.Pago;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,27 +21,39 @@ public class PagoMapper implements Mapper<Pago, PagoDTO> {
 
     @Override
     public PagoDTO toDto(Pago ent) {
-        return new PagoDTO(
-        ent.getIdpago(),
-        ent.getCustomer(),
-        ent.getFecha(),
-        ent.getSubtotal(),
-        ent.getImpuesto(),
-         ent.getTotal()
-        );
+      if (ent == null){
+          return null;
+      }
+      return new PagoDTO(
+      ent.getIdpago(),
+              ent.getCustomer(),
+              ent.getFecha(),
+              ent.getSubtotal(),
+              ent.getImpuesto(),
+              ent.getTotal()
+      );
     }
 
     @Override
     public Pago toEntity(PagoDTO dto) {
-         if(dto == null) return null;
-         return new Pago(
-         dto.getIdPago(),
-         dto.getCustomer(),
-         dto.getFecha(),
-         dto.getSubtotal(),
-         dto.getImpuesto(),
-                 dto.getTotal()
-         );
+        if (dto == null){
+            return null;
+        }
+        try{
+            return new Pago(
+            dto.getIdPago(),
+                    new CustomerMapper().toEntity(new CustomerDAO(Database.getConnection()).read(dto.getCustomer())),
+                    dto.getFecha(),
+                    dto.getSubtotal(),
+                    dto.getImpuesto(),
+                    dto.getTotal()
+            );
+        } catch (SQLException ex) {
+            Logger.getLogger(PagoMapper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(PagoMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
     
 }
