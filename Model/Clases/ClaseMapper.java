f1f7d0b48eb46d5bc96b4d842f8997267c19b2ae
FiaHZ <soherrerazu@est.utn.ac.cr>
2024-11-29 17:47:02 -0600

    solucion error

--- Model/Clases/ClaseMapper.java
+++ Model/Clases/ClaseMapper.java
@@ -6,8 +6,10 @@ package Model.Clases;
 
 import Database.Database;
 import Model.Entrenadores.EntrenadorDAO;
+import Model.Entrenadores.EntrenadorDTO;
 import Model.Entrenadores.EntrenadorMapper;
 import Model.Mapper.Mapper;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -37,16 +39,26 @@ public class ClaseMapper implements Mapper<Clase, ClaseDTO> {
             return null;
         }
         try {
+            
+                   // Obtén la instancia de Database usando el patrón Singleton
+            Database database = Database.getInstance();
+
+            // Usa el método getConnection() de la instancia
+            Connection connection = database.getConnection();
+
+            // Recupera el cliente utilizando el DAO
+            EntrenadorDAO entrenadorDAO = new EntrenadorDAO(connection);
+            
+            EntrenadorDTO entrenador = entrenadorDAO.read(dto.getEntrenadorId());
+            
             return new Clase(
                     dto.getId(),
                     dto.getTipoClase(),
                     dto.getHorario(),
-                    new EntrenadorMapper().toEntity(new EntrenadorDAO(Database.getConnection()).read(dto.getEntrenadorId())) ,
+                    new EntrenadorMapper().toEntity(entrenador),
                     dto.getCapacidadMaxima()
             );
         } catch (SQLException ex) {
-            Logger.getLogger(ClaseMapper.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
             Logger.getLogger(ClaseMapper.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
